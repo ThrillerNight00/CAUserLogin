5e931b3b0404df38fbbe833879b4f20af20b1461
gillali <ali.gill@mail.utoronto.ca>
2024-10-18 14:46:31 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Javadoc.
+     * @param name Takes in name.
+     */
+    void setCurrentUser(String name);
 }
